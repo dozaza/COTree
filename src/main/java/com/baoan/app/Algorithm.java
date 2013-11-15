77deb6920387e8d3576c9157701419c30b0c33a6
baoan.feng <baoan@baoan-Datayes.(none)>
2013-11-15 17:08:17 +0800

    add addElement and test passed

--- src/main/java/com/baoan/app/Algorithm.java
+++ src/main/java/com/baoan/app/Algorithm.java
@@ -1,6 +1,6 @@
 package com.baoan.app;
 
-import java.util.Collection;
+import java.util.List;
 
 /**
  * Editor: intellij@Ubuntu
@@ -9,21 +9,24 @@ import java.util.Collection;
  * Time: 9:54 PM
  */
 class Algorithm {
-    static int binarySearch(Collection collection, Comparable object) throws NotComparableClassException {
-        if ( null == collection || collection.isEmpty() ) {
+    static int binarySearch(List list, Comparable object) throws NotComparableClassException {
+        if ( null == list || list.isEmpty() ) {
             return -1;
         }
-        Object[] array = collection.toArray();
-        int size = collection.size();
+
+        int size = list.size();
         int i = 0;
         int j = size - 1;
 
         while ( i <= j ) {
             int mid = (j + i)/2;
-            if ( !(array[mid] instanceof Comparable) ) {
-                throw new NotComparableClassException(array[mid].getClass().getName());
+            Object midObject = list.get(mid);
+            if ( !(midObject instanceof Comparable) ) {
+                System.out.println(midObject.getClass().getName());
+                throw new NotComparableClassException(midObject.getClass().getName());
             }
-            Comparable compare = (Comparable) array[mid];
+
+            Comparable compare = (Comparable) midObject;
             if ( 0 == object.compareTo(compare) ) {
                 return mid;
             }
@@ -35,5 +38,44 @@ class Algorithm {
             }
         }
         return -1;
+    }
+
+    static void addElement(List list, Comparable object) throws NotComparableClassException {
+        if ( null == list ) {
+            throw new NullPointerException("The collection is null");
+        }
+        if ( list.isEmpty() ) {
+            list.add(object);
+        }
+
+        int size = list.size();
+        int i = 0;
+        int j  = size - 1;
+
+        while ( j-i > 1 ) {
+            int mid = (j + i)/2;
+            Object midObject = list.get(mid);
+            if ( !(midObject instanceof Comparable) ) {
+                System.out.println(midObject.getClass().getName());
+                throw new NotComparableClassException(midObject.getClass().getName());
+            }
+
+            Comparable compare = (Comparable) midObject;
+            if ( object.compareTo(compare) < 0 ) {
+                j = mid;
+            }
+            else {
+                i = mid;
+            }
+        }
+        if ( object.compareTo(list.get(i)) < 0 ) {
+            list.add(i, object);
+        }
+        else if ( object.compareTo(list.get(j)) > 0 ) {
+            list.add(j + 1, object);
+        }
+        else if ( object.compareTo(list.get(i)) > 0 && object.compareTo(list.get(j)) < 0 )  {
+            list.add(j, object);
+        }
     }
 }
--- src/test/java/com/baoan/app/AlgorithmTest.java
+++ src/test/java/com/baoan/app/AlgorithmTest.java
@@ -33,6 +33,21 @@ class TestObject implements Comparable<TestObject> {
         }
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if ( o == this ) {
+            return true;
+        }
+        if ( null == o ) {
+            return false;
+        }
+        TestObject object = (TestObject) o;
+        if ( data == object.data ) {
+            return true;
+        }
+        return false;
+    }
+
     int getData() {
         return data;
     }
@@ -42,22 +57,30 @@ class TestObject implements Comparable<TestObject> {
     }
 }
 
+class NonComparableObject {
+
+}
+
 public class AlgorithmTest {
-    List<TestObject> mockList = new ArrayList<>();
-    List<TestObject> mockList2 = new ArrayList<>();
-    List<TestObject> mockList3 = new ArrayList<>();
-    List<TestObject> mockList4 = null;
-    List<Object> mockList5 = new ArrayList<>();
+
+
 
     @Test(expected = NotComparableClassException.class)
     public void testSort() throws NotComparableClassException {
+        List<TestObject> mockList = new ArrayList<>();
+        List<TestObject> mockList2 = new ArrayList<>();
+        List<TestObject> mockList3 = new ArrayList<>();
+        List<TestObject> mockList4 = null;
+        List<Object> mockList5 = new ArrayList<>();
+        List<NonComparableObject> mockList6 = new ArrayList<>();
+
         for ( int i = 0; i < 10; i++ ) {
             mockList.add(new TestObject(i));
+            mockList6.add(new NonComparableObject());
         }
-
         mockList2.add(new TestObject(1));
-
         mockList5.add(new Object());
+
 
         Assert.assertEquals(2, Algorithm.binarySearch(mockList, new TestObject(2)));
         Assert.assertEquals(9, Algorithm.binarySearch(mockList, new TestObject(9)));
@@ -68,7 +91,54 @@ public class AlgorithmTest {
         Assert.assertEquals(-1, Algorithm.binarySearch(mockList3, new TestObject(1)));
         Assert.assertEquals(-1, Algorithm.binarySearch(mockList4, new TestObject(1)));
 
-        Assert.assertEquals(-1, Algorithm.binarySearch(mockList5, new TestObject(1)));
+        Algorithm.binarySearch(mockList6, new TestObject(1));
+
+        Algorithm.binarySearch(mockList5, new TestObject(1));
+
+
+    }
+
+    @Test(expected = NotComparableClassException.class)
+    public void testAddElement() throws NotComparableClassException {
+        List<TestObject> mockList = new ArrayList<>();
+        List<TestObject> mockList2 = new ArrayList<>();
+        List<TestObject> mockList3 = new ArrayList<>();
+        List<TestObject> mockList4 = new ArrayList<>();
+        List<TestObject> result = new ArrayList<>();
+        List<TestObject> result2 = new ArrayList<>();
+        List<TestObject> result3 = new ArrayList<>();
+        List<TestObject> result4 = new ArrayList<>();
+
+        List<Object> mockList5 = new ArrayList<>();
+        List<NonComparableObject> mockList6 = new ArrayList<>();
+
+        for ( int i = 0; i < 10; i++ ) {
+            mockList.add(new TestObject(i*2));
+            mockList2.add(new TestObject(i*2));
+            mockList3.add(new TestObject(i*2));
+            mockList4.add(new TestObject(i*2));
+            result.add(new TestObject(i*2));
+            result2.add(new TestObject(i*2));
+            result3.add(new TestObject(i*2));
+            result4.add(new TestObject(i*2));
+            mockList6.add(new NonComparableObject());
+        }
+        result.add(2, new TestObject(3));
+        result2.add(4, new TestObject(7));
+        result3.add(10, new TestObject(20));
+        result4.add(0, new TestObject(-2));
+
+        Algorithm.addElement(mockList, new TestObject(3));
+        Algorithm.addElement(mockList2, new TestObject(7));
+        Algorithm.addElement(mockList3, new TestObject(20));
+        Algorithm.addElement(mockList4, new TestObject(-2));
+
+        Assert.assertArrayEquals(result.toArray(), mockList.toArray());
+        Assert.assertArrayEquals(result2.toArray(), mockList2.toArray());
+        Assert.assertArrayEquals(result3.toArray(), mockList3.toArray());
+        Assert.assertArrayEquals(result4.toArray(), mockList4.toArray());
+
+        Algorithm.addElement(mockList6, new TestObject(1));
     }
 
 }
